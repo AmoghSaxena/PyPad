1f88882475cbeb468f19a9e0e1c4ee46c26ff9e5
Amogh Saxena <amoghsaxena1@gmail.com>
2024-04-25 23:31:29 +0100

    First Commit

---
+++ PyPad.py
@@ -0,0 +1,54 @@
+
+from tkinter import *
+from tkinter import filedialog
+
+root=Tk("PyPad")
+root.title("PyPad")
+text=Text(root) 
+text.grid() 
+def saveas():
+
+    global text
+
+    t = text.get("1.0", "end-1c")
+
+    savelocation = filedialog.asksaveasfilename()
+
+    file1=open(savelocation, "w+")
+
+    file1.write(t)
+
+    file1.close()
+button=Button(root, text="Save", command=saveas) 
+button.grid()
+def FontHelvetica():
+
+    global text
+
+    text.config(font="Helvetica")
+def FontCourier():
+
+    global text
+
+    text.config(font="Courier")
+def FontArial():
+
+    global text
+
+    text.config(font="Arial")
+
+font=Menubutton(root, text="Font") 
+font.grid() 
+font.menu=Menu(font, tearoff=0) 
+font["menu"]=font.menu
+Helvetica=IntVar() 
+arial=IntVar() 
+times=IntVar() 
+Courier=IntVar()
+font.menu.add_checkbutton(label="Courier", variable=Courier, 
+command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=Helvetica,
+command=FontHelvetica)
+font.menu.add_checkbutton(label="Arial", variable=arial,
+command=FontArial)
+root.mainloop()
