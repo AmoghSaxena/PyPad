b3cf12148ee2850ecc5ecea8925ca7afbdebca94
Amogh Saxena <amoghsaxena1@gmail.com>
2024-04-26 09:44:45 +0100

    added font style and redesign button

--- PyPad.py
+++ PyPad.py
@@ -1,54 +1,85 @@
-
 from tkinter import *
+from tkinter import font
 from tkinter import filedialog
 
-root=Tk("PyPad")
+root = Tk("PyPad")
 root.title("PyPad")
-text=Text(root) 
-text.grid() 
+root.attributes('-alpha', 0.9)
+
+text = Text(root, wrap='word', undo=True)
+text.grid(sticky='nsew')
+
+# Create a font object for text styling
+current_font_family = 'Arial'
+current_font_size = 12
+text_font = font.Font(family=current_font_family, size=current_font_size)
+text.configure(font=text_font)
+
+def change_font_for_selection(font_family):
+    global current_font_family
+    current_font_family = font_family
+    selected_text = text.tag_ranges(SEL)
+    if selected_text:
+        text.tag_add("selected_font", "sel.first", "sel.last")
+        text.tag_configure("selected_font", font=(font_family, current_font_size))
+
+
+def toggle_bold():
+    current_tags = text.tag_names("sel.first")
+    if "bold" in current_tags:
+        text.tag_remove("bold", "sel.first", "sel.last")
+    else:
+        text.tag_add("bold", "sel.first", "sel.last")
+        text.tag_configure("bold", font=(current_font_family, current_font_size, 'bold'))
+
+def toggle_italic():
+    current_tags = text.tag_names("sel.first")
+    if "italic" in current_tags:
+        text.tag_remove("italic", "sel.first", "sel.last")
+    else:
+        text.tag_add("italic", "sel.first", "sel.last")
+        text.tag_configure("italic", font=(current_font_family, current_font_size, 'italic'))
+
+def toggle_underline():
+    current_tags = text.tag_names("sel.first")
+    if "underline" in current_tags:
+        text.tag_remove("underline", "sel.first", "sel.last")
+    else:
+        text.tag_add("underline", "sel.first", "sel.last")
+        text.tag_configure("underline", font=(current_font_family, current_font_size, 'underline'))
+
 def saveas():
+    try:
+        t = text.get("1.0", "end-1c")
+        savelocation = filedialog.asksaveasfilename()
+        file1 = open(savelocation, "w+")
+        file1.write(t)
+        file1.close()
+    except:
+        print("File not saved!")
+        pass
 
-    global text
+button_font = font.Font(family='Arial', size=10, weight='bold')
 
-    t = text.get("1.0", "end-1c")
+button_frame = Frame(root)
+button_frame.grid(sticky='ew')
 
-    savelocation = filedialog.asksaveasfilename()
+Button(button_frame, text="Save As", command=saveas, font=button_font, bg='#e05141', fg='#ffffff', activebackground='#ffffff', activeforeground='#e05141', padx=10, pady=5, borderwidth=2).pack(side='right')
+Button(button_frame, text="Bold", command=toggle_bold).pack(side='left')
+Button(button_frame, text="Italic", command=toggle_italic).pack(side='left')
+Button(button_frame, text="Underline", command=toggle_underline).pack(side='left')
 
-    file1=open(savelocation, "w+")
+# Dropdown menu for font selection
+font_menu = Menubutton(button_frame, text="Font")
+font_menu.menu = Menu(font_menu, tearoff=0)
+font_menu["menu"] = font_menu.menu
 
-    file1.write(t)
+fonts = ["Arial", "Courier", "Times New Roman", "Verdana"]
+for f in fonts:
+    font_menu.menu.add_command(label=f, command=lambda f=f: change_font_for_selection(f))
 
-    file1.close()
-button=Button(root, text="Save", command=saveas) 
-button.grid()
-def FontHelvetica():
+font_menu.pack(side='left')
 
-    global text
 
-    text.config(font="Helvetica")
-def FontCourier():
 
-    global text
-
-    text.config(font="Courier")
-def FontArial():
-
-    global text
-
-    text.config(font="Arial")
-
-font=Menubutton(root, text="Font") 
-font.grid() 
-font.menu=Menu(font, tearoff=0) 
-font["menu"]=font.menu
-Helvetica=IntVar() 
-arial=IntVar() 
-times=IntVar() 
-Courier=IntVar()
-font.menu.add_checkbutton(label="Courier", variable=Courier, 
-command=FontCourier)
-font.menu.add_checkbutton(label="Helvetica", variable=Helvetica,
-command=FontHelvetica)
-font.menu.add_checkbutton(label="Arial", variable=arial,
-command=FontArial)
 root.mainloop()
---
+++ changlog.md
@@ -0,0 +1,23 @@
+```
+Changes between 1.0 and 1.1:
+--------------------------------
+
+Platform support changes:
+ * Python 3.8+:
+     - Python 3.8+ is now required
+
+Core:
+ * Added Free Font style
+     - Single font will be used for multiple languages
+ * Optimized the code for the memory usage and thread-counts
+ * Added Bold Italic and Underline Support
+ * Changed the `Save As` button style to make it look better
+ * Added Transperancy mode to make it look better.
+
+
+Changes for 1.0:
+--------------------------------
+
+Core:
+ * Added PyPad code and Updated git
+```
